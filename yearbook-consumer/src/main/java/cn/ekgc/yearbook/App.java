55b26ce8d0f940210af4d2ed2f7b36ea8403e656
薛钰东 <xxxyd0519@163.com>
2022-08-20 19:37:55 +0800

    提交消费者项目

---
+++ yearbook-consumer/src/main/java/cn/ekgc/yearbook/App.java
@@ -0,0 +1,13 @@
+package cn.ekgc.yearbook;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
